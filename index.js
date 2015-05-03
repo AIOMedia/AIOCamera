c53127dabe8029cdabd04e8eca5b1b8aebb66877
Elorfin <penin.axel@gmail.com>
2015-05-03 12:34:40 +0200

    fix public dir

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var proc;
 
-app.use('/', express.static(path.join(__dirname, 'stream')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
