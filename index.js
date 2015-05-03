dea64a88f38c16e93b0593c6fa691c0a2229d04b
Elorfin <penin.axel@gmail.com>
2015-05-03 12:43:09 +0200

    change stream directory location

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ io.on('connection', function (socket) {
         if (Object.keys(sockets).length == 0) {
             app.set('watchingFile', false);
             if (proc) proc.kill();
-            fs.unwatchFile('./stream/image_stream.jpg');
+            fs.unwatchFile(__dirname + '/public/stream/image_stream.jpg');
         }
     });
 
@@ -46,7 +46,7 @@ function stopStreaming() {
     if (Object.keys(sockets).length == 0) {
         app.set('watchingFile', false);
         if (proc) proc.kill();
-        fs.unwatchFile('./stream/image_stream.jpg');
+        fs.unwatchFile(__dirname + '/public/stream/image_stream.jpg');
     }
 }
 
@@ -63,7 +63,7 @@ function startStreaming(io) {
 
     app.set('watchingFile', true);
 
-    fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
+    fs.watchFile(__dirname + '/public/stream/image_stream.jpg', function(current, previous) {
         io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
     });
 }
