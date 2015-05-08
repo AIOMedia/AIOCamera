e36cd58f37de84e785f2029d96a500c1ef835d7e
Elorfin <penin.axel@gmail.com>
2015-05-08 10:43:34 +0200

    clean code

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ var express = require('express');
 var app = express();
 
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var fs = require('fs');
+var io   = require('socket.io')(http);
+var fs   = require('fs');
 var path = require('path');
 
 var RaspiCam = require('raspicam');
 
+// Public directory
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+// Error handling
+app.use(function(err, req, res, next) {
+    if (!err) return next();
+
+    console.log("error!!!");
+    res.send("error!!!");
+
+    http.close();
+
+    // Kill the process if it doesn't close normally
+    setTimeout(function () {
+        process.exit(1);
+    }, 3*1000);
+});
+
+// Display User Interface
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile('public/index.html');
 });
 
 var sockets = {};
@@ -20,74 +37,43 @@ var sockets = {};
 // Manage connections of the clients
 io.on('connection', function (socket) {
     sockets[socket.id] = socket;
+    console.log("Total clients connected : ", Object.keys(sockets).length);
 
     // Take a snapshot
     var camera = new RaspiCam({
-        mode: "photo",
-        output: __dirname + '/public/camera/tmp/image.jpg',
-        encoding: "jpg",
+        mode: 'photo',
+        output: 'public/camera/tmp/image.jpg',
+        encoding: 'jpg',
         timeout: 0 // take the picture immediately
     });
 
-    camera.on("started", function( err, timestamp ){
-        console.log("photo started at " + timestamp );
+    camera.on('started', function (err, timestamp) {
+        console.log("photo started at " + timestamp);
     });
 
-    camera.on("read", function( err, timestamp, filename ){
-        console.log("photo image captured with filename: " + filename );
+    camera.on('read', function (err, timestamp, filename) {
+        console.log("photo image captured with filename: " + filename);
 
-        // Send image to client
+        // Send image to clients
         io.sockets.emit('snapshot', 'camera/tmp/image.jpg?_t=' + (Math.random() * 100000));
     });
 
-    camera.on("exit", function( timestamp ){
-        console.log("photo child process has exited at " + timestamp );
+    camera.on('stop', function () {
+        console.log('camera stopped');
     });
 
-    /*try {
-        camera.start();
-    } catch (err) {
+    camera.on('exit', function (timestamp) {
+        console.log("photo child process has exited at " + timestamp);
+    });
 
-    }*/
+    /*camera.start();*/
 
     // Bind events to socket
     socket.on('disconnect', function() {
         delete sockets[socket.id];
     });
-
-    socket.on('start-stream', function() {
-        startStreaming(io);
-    });
-
-    console.log("Total clients connected : ", Object.keys(sockets).length);
 });
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
-
-function stopStreaming() {
-    if (Object.keys(sockets).length == 0) {
-        app.set('watchingFile', false);
-        if (proc) proc.kill();
-        fs.unwatchFile(__dirname + '/public/stream/image_stream.jpg');
-    }
-}
-
-function startStreaming(io) {
-    /*if (app.get('watchingFile')) {
-        io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-        return;
-    }
-
-    var args = ["-w", "640", "-h", "480", "-o", __dirname + "/public/stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
-    proc = spawn('raspistill', args);
-
-    console.log('Watching for changes...');
-
-    app.set('watchingFile', true);
-
-    fs.watchFile(__dirname + '/public/stream/image_stream.jpg', function(current, previous) {
-        io.sockets.emit('liveStream', 'stream/image_stream.jpg?_t=' + (Math.random() * 100000));
-    });*/
-}
